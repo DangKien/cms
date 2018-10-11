d12eceab8e5b3775353712e8a875162aba5d0332
DangKien96 <dangtrungkien96@gmail.com>
2018-10-11 10:31:34 +0700

    commit

--- public/angularJs/uses/ctrls/settingCtrl.js
+++ public/angularJs/uses/ctrls/settingCtrl.js
@@ -1,126 +1,200 @@
 ngApp.controller('settingCtrl',function($scope, $myNotify, $myBootbox, $myLoader, $settingService, $apply) {
 
-	$scope.data = {
-		users: {},
-		page: {},
-		logo: {},
-		banner_home: {},
-		contact: {},
-		service: {},
-		review: {
-			home_image: []
-		},
-		advantage: {}
-	}
-	$scope.filter = {
-		freetext: ""
-	}
+    $scope.data = {
+        users: {},
+        page: {},
+        logo: {},
+        banner_home: {},
+        contact: {},
+        service: {},
+        review: {
+            home_image: []
+        },
+        advantage: {}
+    }
+    $scope.filter = {
+        freetext: ""
+    }
 
-	$scope.actions = {
-		getLogo: function () {
-			$settingService.action.getSetting().then(function (resp) {
-				angular.forEach(resp.data, function(value, key){
-					if (value.key == 'CONTACT') {
-						$scope.data.contact = value.setting
-					}
-					if (value.key == 'GOOGLE_ANALYTIC') {
-						$scope.data.googleApi = value.setting
-					}
-					if (value.key == 'META') {
-						$scope.data.meta = value.setting
-					}
-				});
-			}, function (error) {
-			})
-		},
-		saveContact: function () {
-			let params = {
-				'setting': JSON.stringify(
-					{
-						'address': $scope.data.contact.address || '',
-						'phone'  : $scope.data.contact.phone || '',
-						'worktime' : $scope.data.contact.worktime || '',
-						'fax' : $scope.data.contact.fax || '',
-						'email': $scope.data.contact.email || '',
-						'fb': $scope.data.contact.fb || '',
-						'google_plus': $scope.data.contact.google_plus || '',
-						'youtube'  : $scope.data.contact.youtube || '',
-						'instagram' : $scope.data.contact.instagram || '',
-						'zalo' : $scope.data.contact.zalo || '',
-						'google_map' : $scope.data.contact.google_map || '',
-						'description' : $scope.data.contact.description || '',
-						'coppyright'  : $scope.data.contact.coppyright || '',
-						
-					}
-				),
-				'key' : 'CONTACT'
-			}
-			$settingService.action.insertSetting(params).then(function (resp){
-				if (resp) {
-					$myNotify.success('Success')
-				}
-			}, function (error) {
-				$myNotify.error('Error')
-			});
-		},
+    $scope.actions = {
+        getLogo: function () {
+            $settingService.action.getSetting().then(function (resp) {
+                angular.forEach(resp.data, function(value, key){
+                    if (value.key == 'LOGO') {
+                        $scope.data.logo = value.setting
+                    }
+                    if (value.key == 'BANNER_HOME') {
+                        $scope.data.banner_home = value.setting
+                    }
+                    if (value.key == 'CONTACT') {
+                        $scope.data.contact = value.setting
+                    }
+                    if (value.key == 'OUR_SERVICE') {
+                        $scope.data.service = value.setting
+                    }
+                    if (value.key == 'REVIEW_HOME') {
+                        $scope.data.review = value.setting
+                        console.log($scope.data.review.home_image);
+                    }
+                    if (value.key == 'ADVANTAGE_HOME') {
+                        $scope.data.advantage = value.setting
+                    }
+                });
+            }, function (error) {
+            })
+        },
 
-		saveMeta: function () {
-			let params = {
-				'setting': JSON.stringify(
-					{
-						'title': $scope.data.meta.title || '',
-						'meta_title'  : $scope.data.meta.meta_title || '',
-						'meta_desciption' : $scope.data.meta.meta_description || '',
-						'meta_keyword' : $scope.data.meta.meta_keyword || '',
-					}
-				),
-				'key' : 'META'
-			}
-			$settingService.action.insertSetting(params).then(function (resp){
-				if (resp) {
-					$myNotify.success('Success')
-				}
-			}, function (error) {
-				$myNotify.error('Error')
-			});
-		},
+        changeLogo: function () {
+            let params = {
+                'setting': JSON.stringify({'url_image': $scope.data.logo.url_image}),
+                'key' : 'LOGO'
+            }
+            $settingService.action.insertSetting(params).then(function (resp){
+                if (resp) {
+                    $myNotify.success('Success')
+                }
+            }, function (error) {
+                $myNotify.error('Error')
+            });
+        },
 
-		saveGgAnalytic: function () {
-			let params = {
-				'setting': JSON.stringify(
-					{
-						'google_analytic': $scope.data.googleApi.google_analytic || '',
-					}
-				),
-				'key' : 'GOOGLE_ANALYTIC'
-			}
-			$settingService.action.insertSetting(params).then(function (resp){
-				if (resp) {
-					$myNotify.success('Success')
-				}
-			}, function (error) {
-				$myNotify.error('Error')
-			});
-		},
+        changeImageservcie: function () {
+            let params = {
+                'setting': JSON.stringify({'tool': $scope.data.service.tool, 'pp': $scope.data.service.pp}),
+                'key' : 'OUR_SERVICE'
+            }
+            $settingService.action.insertSetting(params).then(function (resp){
+                if (resp) {
+                    $myNotify.success('Success')
+                }
+            }, function (error) {
+                $myNotify.error('Error')
+            });
+        },
 
-		delete: function ($id) {
-			if ($id) {
-				$myBootbox.confirm('Are you sure?', function (resp) {
-					if (resp) {
-					$settingService.action.deleteUser($id).then(function (resp) {
-						if (resp) {
-							$myNotify.success('Sure!');
-							$scope.actions.getAboutTeam();
-						}
-						}, function (error) {
-							$myNotify.error('No!');
-						})
-					}
-				})
-			}
-		},
+        changeBannerHome: function () {
+            let params = {
+                'setting': JSON.stringify(
+                    {'url_image': $scope.data.banner_home.url_image || '',
+                        'intro': $scope.data.banner_home.intro || '',
+                        'title': $scope.data.banner_home.title || '' }),
+                'key' : 'BANNER_HOME'
+            }
+            $settingService.action.insertSetting(params).then(function (resp){
+                if (resp) {
+                    $myNotify.success('Success')
+                }
+            }, function (error) {
+                $myNotify.error('Error')
+            });
+        },
 
-	}
+        changeImageReview: function () {
+            var arrayVal = [];
 
-	$scope.actions.getLogo();
+            $('.value-image').each(function () {
+                if ($(this).val() != "") {
+                    arrayVal.push($(this).val());
+                }
+            })
+            let params = {
+                'setting': JSON.stringify(
+                    {'home_image': arrayVal || ''}),
+                'key' : 'REVIEW_HOME'
+            }
+            $settingService.action.insertSetting(params).then(function (resp){
+                if (resp) {
+                    $myNotify.success('Success')
+                }
+            }, function (error) {
+                $myNotify.error('Error')
+            });
+        },
+
+        changeImageAdvantage: function () {
+            let params = {
+                'setting': JSON.stringify(
+                    {
+                        'home_image': $scope.data.advantage.home_image || '',
+                        'pp_image' : $scope.data.advantage.pp_image || ''
+                    }),
+                'key' : 'ADVANTAGE_HOME'
+            }
+            $settingService.action.insertSetting(params).then(function (resp){
+                if (resp) {
+                    $myNotify.success('Success')
+                }
+            }, function (error) {
+                $myNotify.error('Error')
+            });
+        },
+
+        saveBanner: function () {
+            let params = {
+                'setting': JSON.stringify(
+                    {
+                        'right_banner': $scope.data.banner.right_banner || '',
+                        'top_banner' : $scope.data.banner.top_banner || ''
+                    }),
+                'key' : 'BANNER'
+            }
+            $settingService.action.insertSetting(params).then(function (resp){
+                if (resp) {
+                    $myNotify.success('Success')
+                }
+            }, function (error) {
+                $myNotify.error('Error')
+            });
+        },
+
+        saveContact: function () {
+            let params = {
+                'setting': JSON.stringify(
+                    {
+                        'address': $scope.data.contact.address || '',
+                        'phone'  : $scope.data.contact.phone || '',
+                        'worktime' : $scope.data.contact.worktime || '',
+                        'fax' : $scope.data.contact.fax || '',
+                        'email': $scope.data.contact.email || '',
+                        'fb': $scope.data.contact.fb || '',
+                        'youtube'  : $scope.data.contact.youtube || '',
+                        'instagram' : $scope.data.contact.instagram || '',
+                        'zalo' : $scope.data.contact.zalo || '',
+                        'google_map' : $scope.data.contact.google_map || '',
+                        'google_analytic': $scope.data.contact.google_analytic || '',
+                        'fb_pixel' : $scope.data.contact.fb_pixel
+                    }
+                ),
+                'key' : 'CONTACT'
+            }
+            console.log(params)
+            $settingService.action.insertSetting(params).then(function (resp){
+                if (resp) {
+                    $myNotify.success('Success')
+                }
+            }, function (error) {
+                $myNotify.error('Error')
+            });
+        },
+
+        delete: function ($id) {
+            if ($id) {
+                $myBootbox.confirm('Are you sure?', function (resp) {
+                    if (resp) {
+                        $settingService.action.deleteUser($id).then(function (resp) {
+                            if (resp) {
+                                $myNotify.success('Sure!');
+                                $scope.actions.getAboutTeam();
+                            }
+                        }, function (error) {
+                            $myNotify.error('No!');
+                        })
+                    }
+                })
+            }
+        },
+
+    }
+
+    $scope.actions.getLogo();
 });
